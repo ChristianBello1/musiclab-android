d493ada6ce1816ba98a1a707ab73b283954a14c0
Christianbello1 <lulabchannel2@gmail.com>
2025-08-10 04:41:04 +0200

    primo test

---
+++ app/src/main/kotlin/com/example/musiclab/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.musiclab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: Implementare scanner file musicali
+        // TODO: Implementare player musicale
+        // TODO: Implementare integrazione Google Drive
+    }
+}
